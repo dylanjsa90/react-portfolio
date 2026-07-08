8425566765a77183b41c26a6a3fba97425825a30
Dylan Sanders <dylanjsanders1@gmail.com>
2026-07-07 19:52:54 -0700

    remove sub-repo

--- snake-game/pages/home.tsx
+++
@@ -1,5 +0,0 @@
-import { SnakeGame } from '@/components/snake-game'
-
-export function HomePage() {
-  return <SnakeGame />
-}
--- snake-game/src/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-    ENVIRONMENT: import.meta.env.VITE_ENV || 'development',
-    GA: import.meta.env.VITE_GA_TRACKING_ID || null,
-}
-
-export const AppConfig = {
-    ...config
-};
